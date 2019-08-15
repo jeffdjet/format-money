85adcd5a739a684ff14a0c60ce51a3748c42d13f
jeffdjet <jeffrey_givera@yahoo.com>
2019-08-15 19:33:34 +0800

    add id attribute in label of new format

--- cypress/integration/money.spec.js
+++ cypress/integration/money.spec.js
@@ -4,4 +4,11 @@ context('Assertions', () => {
     beforeEach(() => {
       cy.visit('http://localhost:3000')
     })
+
+    describe('Assert entered number value', () => {
+        it('.should() - make an assertion about the entered number', () => {
+          cy.get('#cypress-money')
+              .type('1600').should('have.value', '1600')
+      })
+    })
 })
